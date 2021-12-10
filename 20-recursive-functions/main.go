d5ea3f01f76ca1d214c826df6f9e90b8dba26c57
albuquerque53 <gabrielalbuquerque-dev@hotmail.com>
2021-12-10 18:59:11 -0300

    20 - Recursive functions

---
+++ 20-recursive-functions/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func fibonacci(position uint) uint {
+    if position <= 1 {
+        return position
+    }
+
+    return fibonacci(position - 2) + fibonacci(position -  1)
+}
+
+func main() {
+    position := uint(10)
+
+    fmt.Println(fibonacci(position))
+}
